04133c69c69c60ad2e488d78e6e29327f8c614df
Gabriel Dziecinny Ferreira <gabidzferreira@gmail.com>
2025-01-30 18:41:01 -0300

    30012025

--- estudo-1/src/app/componentes/parent-data/parent-data.component.ts
+++ estudo-1/src/app/componentes/parent-data/parent-data.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-parent-data',
+  standalone: true,
   imports: [],
   templateUrl: './parent-data.component.html',
   styleUrl: './parent-data.component.css'
