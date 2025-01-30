2f18b96d8672fa7ef7c3a910822c852d07950002
Gabriel Dziecinny Ferreira <gabidzferreira@gmail.com>
2025-01-30 19:22:31 -0300

    Utilizando as rotas com link

--- estudo-1/src/app/app.component.ts
+++ estudo-1/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentDataComponent } from './componentes/parent-data/parent-data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  userName = 'PatrickPepe';
   title = 'estudo-1';
 }
--- estudo-1/src/app/componentes/parent-data/parent-data.component.ts
+++ estudo-1/src/app/componentes/parent-data/parent-data.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-parent-data',
@@ -7,6 +7,5 @@ import { Component } from '@angular/core';
   templateUrl: './parent-data.component.html',
   styleUrl: './parent-data.component.css'
 })
-export class ParentDataComponent {
-
+export class ParentDataComponent  {
 }
