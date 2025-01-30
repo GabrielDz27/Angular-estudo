befb4c18303165964b55c679fe0af7e51d424020
Gabriel Dziecinny Ferreira <gabidzferreira@gmail.com>
2025-01-30 12:25:34 -0300

    Utilizando as rotas do angular 19

--- estudo-1/src/app/app.component.ts
+++ estudo-1/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FirstComponentComponent } from './components/first-component/first-component.component';
- 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FirstComponentComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- estudo-1/src/app/app.routes.ts
+++ estudo-1/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {path:'', component: FirstComponentComponent}
+
+];
