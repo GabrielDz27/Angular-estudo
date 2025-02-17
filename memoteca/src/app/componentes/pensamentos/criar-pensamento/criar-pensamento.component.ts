af91a4d7ee4ff6ac96efdd8b9aef84f19adc2c9a
Gabriel Dziecinny Ferreira <gabidzferreira@gmail.com>
2025-02-17 17:28:50 -0300

    dados bidirecional
    Aprendendo como aplicação

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -12,7 +12,7 @@ export class CriarPensamentoComponent {
     id: '1',
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
-    modelo: ''
+    modelo: 'modelo1'
   }
   criarPensamento() {
     alert("Novo pensamento criado")
